2e075b3134f4da21ef5c2e9d807d96daf8f6d0f8
Jason Sultana <jason.sultana01@gmail.com>
2023-08-10 18:47:04 +1000

    Add counter button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'github_demo';
+  count: number = 0;
+
+  addOne() {
+    this.count++;
+  }
 }
